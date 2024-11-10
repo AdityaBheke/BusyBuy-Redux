efad93248a0e201ccdf53c598df31c4540a898fc
Aditya Bheke <adityabheke34@gmail.com>
2024-11-11 00:06:42 +0530

    create necessary components and complete navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import style from "./navbar.module.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className={style.navbar}>
+      <Link to={"/"}><div className={style.brandInfo}>
+        Busy Buy
+      </div></Link>
+      <ul className={style.navContainer}>
+        <Link to={"/"}><li><i className="fi fi-ss-house-chimney"></i> Home</li></Link>
+        <Link to={"/"}><li><i className="fi fi-ss-basket-shopping-simple"></i> My Orders</li></Link>
+        <Link to={"/"}><li><i className="fi fi-ss-shopping-cart"></i> Cart</li></Link>
+        <Link to={"/"}><li><i className="fi fi-br-exit"></i> Logout</li></Link>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
