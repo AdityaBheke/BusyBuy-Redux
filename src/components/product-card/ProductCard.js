0f90791817bc56354f81f00cee13f559e7440ca1
Aditya Bheke <adityabheke34@gmail.com>
2024-11-13 15:19:49 +0530

    handle navigation for signed out user

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import { useUserValue } from "../../context/userContext";
 import styles from "./productCard.module.css";
 
 function ProductCard(props) {
   const {title, price, image} = props.product;
-  const {handleAddToCart} = useUserValue();
+  const {handleAddToCart, isLoggedIn} = useUserValue();
+  const navigate = useNavigate();
   return (
     <div className={styles.card}>
       <img
@@ -16,7 +18,7 @@ function ProductCard(props) {
         <span className={styles.price}>${price}</span>
       </div>
 
-      <button className={styles.addToCart} onClick={()=>{handleAddToCart(props.product)}}>Add to cart</button>
+      <button className={styles.addToCart} onClick={()=>{isLoggedIn?handleAddToCart(props.product):navigate("/signin")}}>Add to cart</button>
     </div>
   );
 }
